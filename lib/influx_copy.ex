c0199e7c08c79f8b5e9ca372e9148fe8ddc039d3
techgaun <coolsamar207@gmail.com>
2016-07-25 02:12:24 -0500

    runtime config update for src and dest

--- lib/influx_copy.ex
+++ lib/influx_copy.ex
@@ -1,4 +1,10 @@
 defmodule InfluxCopy do
+  @moduledoc """
+  Module entrypoint for influx copy script
+  """
+
+  alias InfluxCopy.{SrcConn, DestConn}
+  require Logger
   @conn_error "connection string invalid. Format example: https://user:pass@host:port/db:measurement"
   def main(args \\ []) do
     {opts, _, _} = OptionParser.parse(args,
@@ -18,6 +24,22 @@ defmodule InfluxCopy do
     case Enum.count(errors) do
       0 ->
         IO.puts "Performing the copy..."
+        Supervisor.start_link(
+          [ SrcConn.child_spec ],
+          strategy: :one_for_one
+        )
+        Supervisor.start_link(
+          [ DestConn.child_spec ],
+          strategy: :one_for_one
+        )
+
+        {src_config, src_db, src_measurement} = get_influx_config(SrcConn.config, src)
+        {dest_config, dest_db, dest_measurement} = get_influx_config(DestConn.config, dest)
+        :ok = Application.put_env(:influx_copy, SrcConn, src_config)
+        :ok = Application.put_env(:influx_copy, DestConn, dest_config)
+        Logger.warn inspect InfluxCopy.SrcConn.config
+        Logger.warn inspect InfluxCopy.DestConn.config
+
       _ ->
         errors
         |> Enum.each(fn x ->
@@ -37,4 +59,14 @@ defmodule InfluxCopy do
     end
   end
   def parse_conn(_), do: nil
+
+  def get_influx_config(config, conn_list) do
+    [scheme, user, pass, host, port, db, measurement] = conn_list
+    new_config = config
+      |> Keyword.put(:host, host)
+      |> Keyword.put(:scheme, scheme)
+      |> Keyword.put(:port, port)
+      |> Keyword.put(:auth, [username: user, password: pass])
+    {new_config, db, measurement}
+  end
 end
