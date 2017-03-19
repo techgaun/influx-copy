68938985f3a07c09ae3c132baff78fa0ad731588
techgaun <coolsamar207@gmail.com>
2017-03-18 21:19:51 -0500

    update deps

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule InfluxCopy.Mixfile do
     [app: :influx_copy,
      version: "0.1.0",
      elixir: "~> 1.3",
-     escript: escript,
+     escript: escript(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
